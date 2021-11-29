ac09d3b27ab7eadec0523c14e8597320f5ade6e7
ZFLin <913632394@qq.com>
2021-11-29 23:02:48 +0800

    UPDATE render函数获取

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import { compileToFunction } from "./Compiler/index"
 import { initState } from "./state"
 
 /**
@@ -5,10 +6,31 @@ import { initState } from "./state"
  * @param {*} Vue 
  */
 export function initMixin(Vue) {
+    // 初始化处理
     Vue.prototype._init = function (options) {
         const vm = this
         vm.$options = options
         initState(vm)
+
+        // 元素挂载完成后，触发生命周期钩子
+        if (vm.$options.el) {
+            vm.$mount(vm.$options.el)
+        }
+    }
+    // 初始化元素绑定
+    Vue.prototype.$mount = function (el) {
+        const vm = this
+        const options = vm.$options
+        el = document.querySelector(el)
+
+        // 获取render函数（优先级 render>template>el）
+        if (!options.render) {
+            let template = options.template
+            if (!template && el) {
+                template = el.outerHTML
+            }
+            options.render = compileToFunction(template)
+        }
     }
 }
 
