f09d6beb8ecafccc53b5b202261df2b7f07b04ae
zflin <flinzhu@gravitylink.com>
2021-12-20 16:51:31 +0800

    UPDATE 12数组更新处理

--- src/Observe/index.js
+++ src/Observe/index.js
@@ -4,6 +4,7 @@ import Dep from "./dep"
 
 class Observer {
     constructor(data) {
+        this.dep = new Dep()
         Object.defineProperty(data, '__ob__', {
             value: this,
             enumerable: false,
@@ -25,35 +26,53 @@ class Observer {
 
     walk(data) {
         Object.keys(data).forEach(key => {
-            this.defineReactive(data, key, data[key])
+            defineReactive(data, key, data[key])
         })
     }
 
-    defineReactive(data, key, value) {
-        observe(value)
-        let dep = new Dep()
-        Object.defineProperty(data, key, {
-            get() {
-                if (Dep.target) {
-                    dep.depend()
-                }
-                return value
-            },
-            set(newVal) {
-                if (newVal === value) return
-                observe(newVal)
-                value = newVal
-                dep.notify()
-                return value
-            }
-        })
-    }
+
+
 }
 
 
+function dependArray(value) {
+    for (let i = 0; i < value.length; i++) {
+        let current = value[i]
+        current.__ob__ && current.__ob__.dep.depend()
+        if (Array.isArray(current)) {
+            dependArray(current)
+        }
+    }
+}
+
+function defineReactive(data, key, value) {
+    let valueOb = observe(value)
+    let dep = new Dep()
+    Object.defineProperty(data, key, {
+        get() {
+            if (Dep.target) {
+                dep.depend()
+                if (valueOb) {
+                    valueOb.dep.depend()
+                    if (Array.isArray(value)) {
+                        dependArray(value)
+                    }
+                }
+            }
+            return value
+        },
+        set(newVal) {
+            if (newVal === value) return
+            observe(newVal)
+            value = newVal
+            dep.notify()
+            return value
+        }
+    })
+}
 
 export function observe(data) {
     if (!isObject(data)) return
-    if (data.__ob__) return
+    if (data.__ob__) return data.__ob__
     return new Observer(data)
 }
